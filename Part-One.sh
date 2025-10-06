a4939181bb0b328781c2eb5fb63f796a2543bc14
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2025-10-05 23:06:23 -0500

    Update Part-One.sh
    Updated Kiss tarbell

--- Part-One.sh
+++ Part-One.sh
@@ -146,7 +146,7 @@ start_installation() {
 
     # Download KISS chroot tarball
     dialog --infobox "Downloading KISS chroot tarball..." 3 40
-    if ! curl --output kiss.xz --fail https://codeberg.org/kiss-community/repo/releases/download/24.06.14/kiss-chroot-24.06.14-2.tar.xz; then
+    if ! curl --output kiss.xz --fail https://codeberg.org/kiss-community/repo/releases/download/24.12.18/kiss-chroot-24.12.18.tar.xz; then
         dialog --title "Error" --msgbox "Failed to download the KISS chroot tarball. Check your internet connection." 6 50
         return
     fi
